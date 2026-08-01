717709f38dc357d9c7ba7849950612f082ce4ffa
atanuroy22 <atanukrroy45@gmail.com>
2026-08-01 20:37:27 +0530

    version increase

--- version.sh
+++ version.sh
@@ -12,10 +12,10 @@
 #   VERSION=2.0.0 VERSION_CODE=2 bash android/build-apk.sh
 
 # Release version (used for all platforms: Linux, Windows, macOS, APK)
-VERSION="${VERSION:-0.15.4}"
+VERSION="${VERSION:-0.15.5}"
 
 # APK version code (must be an integer, incremented with each update)
-VERSION_CODE="${VERSION_CODE:-43}"
+VERSION_CODE="${VERSION_CODE:-44}"
 
 # Export for use in subshells
 export VERSION VERSION_CODE
